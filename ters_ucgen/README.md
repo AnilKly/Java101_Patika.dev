771635cc961662c0755db03ce58438ac6d4c6d0d
Anıl KALAY <91129853+AnilKly@users.noreply.github.com>
2023-07-15 14:14:24 +0300

    Add files via upload

---
+++ ters_ucgen/README.md
@@ -0,0 +1,23 @@
+# Ters Üçgen Çizme Programı
+
+![img](uc.png)
+
+Bu program, kullanıcının girdiği basamak sayısına göre ters üçgen çizen bir Java programıdır.
+
+## Kullanım
+
+1. Programı çalıştırın.
+2. Bir basamak sayısı girin.
+3. Program, girilen basamak sayısına göre ters üçgeni ekranda çizecektir.
+
+## İletişim
+Bana ulaşın [anilkalay8@gmail.com]
+
+## Lisans
+
+Bu proje MIT Lisansı altında lisanslanmıştır. Detaylı lisans metni için [LICENSE](LICENSE) dosyasını inceleyebilirsiniz.
+
+---
+
+Bu program örnektir. Ters üçgenin şekli ve boyutu değiştirilebilir veya özelleştirilebilir.
+
---
+++ ters_ucgen/src/TersUcgen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Basamak Sayısını Girin: ");
+        int basamakSayisi = scanner.nextInt();
+
+        for (int i = basamakSayisi; i >= 1; i--) {
+            for (int j = 1; j <= basamakSayisi - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
